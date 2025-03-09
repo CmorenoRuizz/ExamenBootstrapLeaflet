da75102d7a95c40464c283a23694fec54d924676
CmorenoRuizz <camoru@gmail.com>
2025-03-09 20:26:15 +0100

    Creacion y configuracion de repositorio antes de examen

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/testleaflet', function(req, res, next) {
+  res.render('testleaflet', { title: 'Prueba instalación' });
+});
+
+module.exports = router;
